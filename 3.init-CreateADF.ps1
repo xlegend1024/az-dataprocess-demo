ce9d427fa0552b067193271713955e9ac10a4886
hyun-book <hyssh@hysshsbook.redmond.corp.microsoft.com>
2017-11-01 17:28:31 -0700

    update init cli and ps1

---
+++ 3.init-CreateADF.ps1
@@ -0,0 +1,25 @@
+# Data Process Demo 
+# This script will create demo environment
+# for Adanvaced Data Analtyics
+# for ADF
+
+#Login-AzureRmAccount
+$rgName="bigdata-demo-rg"
+#$loc="eastus2"
+$adfName="bigdatademoadf"
+$myTags="Env=demo"
+
+$adf=New-AzureRmDataFactory -ResourceGroupName $rgName -Name $adfName –Location "West US" 
+New-AzureRmDataFactoryLinkedService $adf -File .\1.ADF\ADFJson\Source-WWIDW.json
+New-AzureRmDataFactoryLinkedService $adf -File .\1.ADF\ADFJson\Destination-WWIDataLake.json
+New-AzureRmDataFactoryLinkedService $adf -File .\1.ADF\ADFJson\AzureStorageLinkedService.json
+
+New-AzureRmDataFactoryDataset $adf -File .\1.ADF\ADFJson\ADLAOutput.json
+New-AzureRmDataFactoryDataset $adf -File .\1.ADF\ADFJson\input-dim-cust.json
+New-AzureRmDataFactoryDataset $adf -File .\1.ADF\ADFJson\input-dim-date.json
+New-AzureRmDataFactoryDataset $adf -File .\1.ADF\ADFJson\input-fact-sale.json
+New-AzureRmDataFactoryDataset $adf -File .\1.ADF\ADFJson\output-dim-cust.json
+New-AzureRmDataFactoryDataset $adf -File .\1.ADF\ADFJson\output-dim-date.json
+New-AzureRmDataFactoryDataset $adf -File .\1.ADF\ADFJson\output-fact-sale.json
+
+New-AzureRmDataFactoryLinkedService $adf -File .\1.ADF\ADFJson\CopyData_WWIDWtoADL.json
--- Readme.md
+++ Readme.md
@@ -1,12 +1,9 @@
-#
+# Setup Environment
 
-## Setup Environment 
 Run following script in order to setup an environment on [Azure portal](https://portal.azure.com).
 To save time use [Cloud Shell](https://azure.microsoft.com/en-us/features/cloud-shell/) on the portal.
+
 1. "0.Init-Create-Resource-Group.azcli"
 1. "1.Init-RestoreDBfromBlob.azcli"
 1. "2.Init-ADFDemoData.azcli"
 1. "3.Init-createAzureDataLake.azcli"
-
-
-
