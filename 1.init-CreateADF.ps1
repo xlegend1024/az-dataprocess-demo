b255bdba3f779799e98a1ebd6c73cdf6a04fbfce
hyun-book <hyssh@hysshsbook.redmond.corp.microsoft.com>
2017-11-27 10:30:08 -0800

    fix ADF and ADLS permission

--- 1.init-CreateADF.ps1
+++ 1.init-CreateADF.ps1
@@ -11,7 +11,6 @@ $clusterName=$params.clusterName
 $adfName=$params.adfName 
 $loc="eastus2"
 $spName="Dataprocess_delete_ME"
-$spNameHDI="Dataprocess_delete_ME_HDI"
 $spPWDHDI="1q2w3e4r5t^Y"
 $certificateFilePath="$HOME\clouddrive\3.HDI\cert-download.pfx"
 $storageAccountName = $adlsName # Data Lake Store account name
@@ -25,34 +24,18 @@ write-host "*** For example, azureuser ***"
 $sshCredentials = Get-Credential
 $tenantID = (Get-AzureRmContext).Tenant.TenantId
 
-$app = (Get-AzureRmADApplication -DisplayName $spName)[0]
+$app = Get-AzureRmADServicePrincipal -SearchString $spName
 $myrootdir = "/"
 New-AzureRmDataLakeStoreItem -Folder -AccountName $adlsName -Path $myrootdir/data
 New-AzureRmDataLakeStoreItem -Folder -AccountName $adlsName -Path $myrootdir/data/output
 New-AzureRmDataLakeStoreItem -Folder -AccountName $adlsName -Path $myrootdir/clusters
-New-AzureRmDataLakeStoreItem -Folder -AccountName $adlsName -Path $myrootdir/clusters/hdiadlcluste
 
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path / -AceType User -Id $app.ApplicationId -Permissions All
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /system -AceType User -Id $app.ApplicationId -Permissions All 
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /catalog -AceType User -Id $app.ApplicationId -Permissions All 
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /data -AceType User -Id $app.ApplicationId -Permissions All
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /data/output -AceType User -Id $app.ApplicationId -Permissions All
-
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path / -AceType Group -Id $app.ApplicationId -Permissions 777
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /system -AceType Other -Id $app.ApplicationId -Permissions All 
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /catalog -AceType Other -Id $app.ApplicationId -Permissions All 
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /data -AceType User -Id $app.ApplicationId -Permissions All
-Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /data/output -AceType User -Id $app.ApplicationId -Permissions All
-
-#Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /clusters -AceType User -Id $app.ApplicationId -Permissions All
-#Set-AzureRmDataLakeStoreItemAclEntry -AccountName $adlsName -Path /clusters/hdiadlcluster -AceType User -Id $app.ApplicationId -Permissions All
-
+Set-AzureRmDataLakeStoreItemOwner -Account $adlsName -Path $myrootdir/data -Type User -Id $app.Id
+Set-AzureRmDataLakeStoreItemOwner -Account $adlsName -Path $myrootdir/data/output -Type User -Id $app.Id
 Set-AzureRmDataLakeStoreItemPermission -Account $adlsName -Path $myrootdir/data -Permission 777
 Set-AzureRmDataLakeStoreItemPermission -Account $adlsName -Path $myrootdir/data/output -Permission 777
-Set-AzureRmDataLakeStoreItemPermission -Account $adlsName -Path $myrootdir/clusters -Permission 777
-Set-AzureRmDataLakeStoreItemPermission -Account $adlsName -Path $myrootdir/clusters/hdiadlcluste -Permission 777
-
-
+Set-AzureRmDataLakeStoreItemAclEntry -Account $adlsName -Path $myrootdir/data -AceType User -Id $app.Id -Permissions All
+Set-AzureRmDataLakeStoreItemAclEntry -Account $adlsName -Path $myrootdir/data/output -AceType User -Id $app.Id -Permissions All
 
 $adf=New-AzureRmDataFactory -ResourceGroupName $rgName -Name $adfName –Location "West US" 
 New-AzureRmDataFactoryLinkedService $adf -File $HOME\CloudDrive\1.ADF\ADFJson\Source-WWIDW.json
@@ -60,24 +43,21 @@ New-AzureRmDataFactoryLinkedService $adf -File $HOME\CloudDrive\1.ADF\ADFJson\US
 New-AzureRmDataFactoryDataset $adf -File $HOME\CloudDrive\1.ADF\ADFJson\input-dim-cust.json
 New-AzureRmDataFactoryDataset $adf -File $HOME\CloudDrive\1.ADF\ADFJson\input-dim-date.json
 New-AzureRmDataFactoryDataset $adf -File $HOME\CloudDrive\1.ADF\ADFJson\input-fact-sale.json
-
 New-AzureRmDataFactoryLinkedService $adf -File $HOME\CloudDrive\1.ADF\ADFJson\Destination-WWIDataLake.json
 New-AzureRmDataFactoryLinkedService $adf -File $HOME\CloudDrive\1.ADF\ADFJson\AzureDataLakeAnalyticsLinkedService.json
 New-AzureRmDataFactoryDataset $adf -File $HOME\CloudDrive\1.ADF\ADFJson\ADLAOutput.json
 New-AzureRmDataFactoryDataset $adf -File $HOME\CloudDrive\1.ADF\ADFJson\output-dim-cust.json
 New-AzureRmDataFactoryDataset $adf -File $HOME\CloudDrive\1.ADF\ADFJson\output-dim-date.json
 New-AzureRmDataFactoryDataset $adf -File $HOME\CloudDrive\1.ADF\ADFJson\output-fact-sale.json
-
 New-AzureRmDataFactoryPipeline $adf -File $HOME\CloudDrive\1.ADF\ADFJson\CopyData_WWIDWtoADL.json
 
 #HDI with ADLS
 $certificatePFX = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certificateFilePath, $spPWDHDI)
 $rawCertificateData = $certificatePFX.GetRawCertData()
 $credential = [System.Convert]::ToBase64String($rawCertificateData)
-$HDIApp = New-AzureRmADApplication -DisplayName $spNameHDI -HomePage "https://contoso.com" -IdentifierUris "https://"$spNameHDI".azurehdinsight.net"  -CertValue $credential  -StartDate $certificatePFX.NotBefore -EndDate $certificatePFX.NotAfter
+$HDIApp = New-AzureRmADApplication -DisplayName "HDIADL" -HomePage "https://contoso.com" -IdentifierUris "https://mycontoso.com" -CertValue $credential -StartDate $certificatePFX.NotBefore -EndDate $certificatePFX.NotAfter
 $HDIAppId = $HDIApp.ApplicationId
 $servicePrincipalHDI = New-AzureRmADServicePrincipal -ApplicationId $HDIAppId
-#$servicePrincipalHDI = Get-AzureRmADServicePrincipal -SearchString $spNameHDI
 Set-AzureRmDataLakeStoreItemAclEntry -AccountName $storageAccountName -Path / -AceType User -Id $servicePrincipalHDI.Id -Permissions All
 Set-AzureRmDataLakeStoreItemAclEntry -AccountName $storageAccountName -Path /clusters -AceType User -Id $servicePrincipalHDI.Id -Permissions All
 Set-AzureRmDataLakeStoreItemAclEntry -AccountName $storageAccountName -Path /clusters/hdiadlcluster -AceType User -Id $servicePrincipalHDI.Id -Permissions All
